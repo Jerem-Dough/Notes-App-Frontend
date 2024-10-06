d0d8e46175a6e2d294d6cfbbc34de0872afe09fb
Jeremy Dougherty <143429873+jerem-dough@users.noreply.github.com>
2024-10-05 18:56:30 -0600

    Removed Comments

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import './index.css';  // Optional: Your custom styles
+import './index.css';  
 
-// Render the root App component to the DOM
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')  // Renders into the div with id 'root' in public/index.html
+  document.getElementById('root') 
 );
